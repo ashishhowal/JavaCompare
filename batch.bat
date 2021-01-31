0250c031fd6d949b26d071e152e267cf268906c5
Gitea <gitea@fake.local>
2021-01-31 13:50:34 +0530

    added batch and properties file

---
+++ batch.bat
@@ -0,0 +1,5 @@
+echo running batch
+set CLASSPATH=.
+set CLASSPATH=%CLASSPATH%;C:/Ashish/Complexity Batch/complexityBatch.jar;C:/Ashish/Complexity Batch/commons-io-2.2.jar
+
+%JAVA_HOME%\bin\java -Xms128m -Xmx384m -Xnoclassgc Howal.FileCompare
