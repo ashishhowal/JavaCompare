bd0ace3065fd4963e62908b2dee0ce8bb3a7de9a
Gitea <gitea@fake.local>
2021-03-16 14:37:01 +0530

    changed batch file, added relative path

--- batch.bat
+++ batch.bat
@@ -1,5 +1,5 @@
 echo running batch
 set CLASSPATH=.
-set CLASSPATH=%CLASSPATH%;C:/Ashish/Complexity Batch/complexityBatch.jar;C:/Ashish/Complexity Batch/commons-io-2.2.jar
+set CLASSPATH=%CLASSPATH%;%~dp0/complexityBatch.jar;%~dp0/commons-io-2.2.jar
 
 %JAVA_HOME%\bin\java -Xms128m -Xmx384m -Xnoclassgc Howal.FileCompare
